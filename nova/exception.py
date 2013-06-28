79f5262e5b1af733d57fe51be745d732ca7bacec
Nikola Dipanov <ndipanov@redhat.com>
2013-06-28 16:04:17 +0200

    Make API part of instance boot use new BDM format
    This patch makes the API part of the instance boot process rely on the
    new BDM format. It adds additional validations that are enabled due to
    using the new format.
    
    Since the new format will allow to specify devices that will be created
    as images on the hypervisor (if the hypervisor enables it), in order
    to prevent a DOS, a new config option 'max_local_block_devices' was
    added. This option allows the operator to set a limit on the number of
    destination_type='local' block devices a user can specify per VM.
    
    This patch also refactors and simplifies how block devices are handled
    during boot, and makes sure that validation errors are caught and
    transformed to expected HTTP errors.
    
    Change-Id: I62e0807fcff0a284ff72555d107cac1c9864e46d
    blueprint: improve-block-device-handling

--- nova/exception.py
+++ nova/exception.py
@@ -228,6 +228,24 @@ class InvalidBDMVolume(InvalidBDM):
                 "failed to get volume %(id)s.")
 
 
+class InvalidBDMImage(InvalidBDM):
+    message = _("Block Device Mapping is Invalid: "
+                "failed to get image %(id)s.")
+
+
+class InvalidBDMBootSequence(InvalidBDM):
+    message = _("Block Device Mapping is Invalid: "
+                "Boot sequence for the instance "
+                "and image/block device mapping "
+                "combination is not valid.")
+
+
+class InvalidBDMLocalsLimit(InvalidBDM):
+    message = _("Block Device Mapping is Invalid: "
+                "You specified more local devices than the "
+                "limit allows")
+
+
 class InvalidBDMFormat(InvalidBDM):
     msg_fmt = _("Block Device Mapping is Invalid: "
                 "%(details)s")
