bcb718de04cde1382de2df3241bf4e74a538335c
Nikola Dipanov <ndipanov@redhat.com>
2013-07-15 10:40:50 +0200

    Change RPC to use new BDM format for instance boot
    This patch makes rpc calls that are the result of booting an instance
    propagate the new block device mapping format.
    
    The way this is done is by adding a 'legacy_bdm' flag to the conductor
    (task) API. Since both the scheduler and the compute service propagate
    the block device mapping as part of the request_spec field, a flag named
    legacy_bdm_in_spec to indicate that was introduced in their respective
    APIs.
    
    Currently block_device_mapping is not used by any of the in-tree filters
    in the scheduler, so it is assumed that scheduler's boot functionality
    has now transitioned to the new format.
    
    This patch also bumps RPC versions of the task, scheduler and compute
    manager classes.
    
    Finally this patch propagates the legacy_bdm_in_spec through the
    run_instance callback of the compute manager so that, in case of error -
    the instance is re-scheduled with the proper flag set.
    
    blueprint: improve-block-device-handling
    
    Change-Id: I5f25ddd4d586dda91061f065c1796be726b0ede3

--- nova/compute/rpcapi.py
+++ nova/compute/rpcapi.py
@@ -186,6 +186,7 @@ class ComputeAPI(nova.openstack.common.rpc.proxy.RpcProxy):
         2.32 - Make reboot_instance take a new world instance object
         2.33 - Made suspend_instance() and resume_instance() take new-world
                instance objects
+        2.34 - Added the leagacy_bdm_in_spec parameter to run_instance
     '''
 
     #
@@ -579,15 +580,16 @@ class ComputeAPI(nova.openstack.common.rpc.proxy.RpcProxy):
     def run_instance(self, ctxt, instance, host, request_spec,
                      filter_properties, requested_networks,
                      injected_files, admin_password,
-                     is_first_time, node=None):
+                     is_first_time, node=None, legacy_bdm_in_spec=True):
         instance_p = jsonutils.to_primitive(instance)
         self.cast(ctxt, self.make_msg('run_instance', instance=instance_p,
                 request_spec=request_spec, filter_properties=filter_properties,
                 requested_networks=requested_networks,
                 injected_files=injected_files, admin_password=admin_password,
-                is_first_time=is_first_time, node=node),
+                is_first_time=is_first_time, node=node,
+                legacy_bdm_in_spec=legacy_bdm_in_spec),
                 topic=_compute_topic(self.topic, ctxt, host, None),
-                version='2.19')
+                version='2.34')
 
     def set_admin_password(self, ctxt, instance, new_pass):
         instance_p = jsonutils.to_primitive(instance)
